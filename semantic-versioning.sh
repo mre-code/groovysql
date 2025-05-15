d375d33e2decb7dbe7cfc2b1be610d67d6150416
Steve Etchelecu <169301962+mre-code@users.noreply.github.com>
2025-05-14 22:58:00 -0700

    Semantic versioning:
    added show/update/reset actions to semantic_versioning.sh

--- semantic-versioning.sh
+++ semantic-versioning.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
 
+# gitversion inspects the current git state and calculates a semantic version number
+# https://semver.org
+# https://gitversion.net
+
 function errexit { echo "ERROR: $*" ; exit 255 ; }
 
 function get_version {
-	STR=$1
-	FILE=$2
-	grep --with-filename "$STR" $FILE
+	STR="$1"
+	FILE="$2"
+	echo ">>> $FILE:"
+	grep --line-number "$STR" $FILE
+	echo
 }
 
 function set_version {
-	OLD=$1
-	NEW=$2
-	FILE=$3
-
-	echo; echo "updating $FILE"
-
+	OLD="$1"
+	NEW="$2"
+	FILE="$3"
+	echo ">>> $FILE:"
 	sed --in-place "s/$OLD/$NEW/" $FILE
-
-	grep "$NEW" $FILE | sed 's/^/>>>> /'
-
+	grep --line-number "$NEW" $FILE # | sed 's/^/>>>> /'
+	echo
 }
 
 function update_version {
+	# easiest implementation requires that gitversion be run
+	# at the top of the git work tree (where the .git folder is)
 	if [[ -d .git ]]
 	then NEWVERSION=$(gitversion -showvariable fullsemver 2>/dev/null)
 	else errexit "no .git directory found"
 	fi
 
 	if [[ $NEWVERSION ]]
-	then echo "setting version to $NEWVERSION"
+	then echo "setting version to $NEWVERSION"; echo
 	else errexit "gitversion did not return a version"
 	fi
 
@@ -43,6 +48,7 @@ function reset_version {
 	OLDVERSION="$1"
 	NEWVERSION=SEMANTIC_VERSION
 
+	echo "setting version to $NEWVERSION"; echo
 	set_version "Version $OLDVERSION" "Version $NEWVERSION" README.yaml
 	set_version "version=$OLDVERSION" "version=$NEWVERSION" gradle.properties
 	set_version "GroovySQL $OLDVERSION" "GroovySQL $NEWVERSION" src/main/groovy/net/venturechain/database/Connection.groovy
@@ -61,6 +67,6 @@ function show_version {
 case $1 in
 	show)	show_version ;;
 	update)	update_version ;;
-	reset)	reset_version "$2" ;;
+	reset)	reset_version "${2?"from-version required"}" ;;
 	*)	echo "Usage: $0 { show | update | reset }" ;;
 esac
